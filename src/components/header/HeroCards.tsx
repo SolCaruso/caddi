26c8214afddd213ad64d5a4c8a04152ea192699c
SolCaruso <caruso@ocelot.ltd>
2025-07-13 19:49:43 -0400

    polishes hero cards

--- src/components/header/HeroCards.tsx
+++ src/components/header/HeroCards.tsx
@@ -8,7 +8,7 @@ export default function HeroCards() {
 
             {/* Left Card */}
             <div
-                className="relative flex-1 rounded-xl overflow-hidden flex ml-4 mr-2 my-4 w-full max-h-[850px]"
+                className="relative md:flex-1 rounded-lg lg:rounded-xl overflow-hidden flex mx-2 mt-2 md:ml-4 md:mr-2 md:my-4 md:w-full h-[500px] sm:h-[350px] xl:h-[450px] 3xl:h-[550px] 4xl:h-[650px] 5xl:h-[750px]"
                 style={{
                 backgroundImage: 'url(/webm/trophy-bg.webp)',
                 backgroundSize: 'cover',
@@ -19,18 +19,18 @@ export default function HeroCards() {
                 <div className="absolute inset-0 bg-gradient-to-r from-black/100 to-transparent"/>
 
                 {/* Content */}
-                <div className="relative z-10 flex justify-between items-end w-full p-6 pr-42">
+                <div className="relative z-10 flex justify-between items-end w-full pl-4 pr-2 pb-10 xl:p-6">
 
                     {/* Text & Button */}
-                    <div className='text-white p-6 flex flex-col justify-between h-full'>
+                    <div className='text-white xl:p-6 flex flex-col justify-between h-full'>
 
-                        <div className='h-full w-full'>
+                        <div className='h-full w-full hidden md:block'>
                            {/* Spacer Box */}
                         </div>
                         
                         <div className='h-full w-full flex flex-col justify-end'>
                             <p className='text-lg font-semibold mb-2'>Custom Golf Course Materials & Trophies</p>
-                            <h2 className='text-5xl font-family-proxima-nova-extra-condensed font-extrabold mb-6 uppercase text-nowrap'>Crafted for the Course</h2>
+                            <h2 className='text-4xl 3xl:text-5xl font-family-proxima-nova-extra-condensed font-extrabold mb-8 uppercase'>Crafted for the Course</h2>
                             <div>
                                 <Link href='/' className='bg-white hover:bg-gray-200 transition-all duration-100 ease-in-out-quad text-black px-8 py-2 rounded-full font-semibold cursor-pointer'>
                                     Learn More
@@ -46,7 +46,7 @@ export default function HeroCards() {
                         alt="Trophy Forecaddi"
                         height={463}
                         width={309}
-                        className=""
+                        className="w-[250px] h-auto 3xl:w-[309px] 4xl:w-[359px] hidden 2xl:block mr-12 3xl:mr-24"
                     />
                     
                 </div>
@@ -55,7 +55,7 @@ export default function HeroCards() {
 
             {/* Right Card */}
             <div
-                className="relative flex-1 rounded-xl overflow-hidden flex mr-4 ml-2 my-4 w-full max-h-[850px]"
+                className="relative md:flex-1 rounded-lg lg:rounded-xl overflow-hidden flex mx-2 mt-2 md:mr-4 md:ml-2 md:my-4 md:w-full h-[500px] sm:h-[350px] xl:h-[450px] 3xl:h-[550px] 4xl:h-[650px] 5xl:h-[750px]"
                 style={{
                 backgroundImage: 'url(/webm/forecaddi-bg.webp)',
                 backgroundSize: 'cover',
@@ -66,18 +66,18 @@ export default function HeroCards() {
                 <div className="absolute inset-0 bg-gradient-to-r from-black/70 to-transparent"/>
 
                 {/* Content */}
-                <div className="relative z-10 flex justify-between items-end w-full p-6">
+                <div className="relative z-10 flex flex-col-reverse sm:flex-row sm:justify-between sm:items-end w-full pl-4 pr-2 pb-10 xl:p-6">
 
                     {/* Text & Button */}
-                    <div className='text-white p-6 flex flex-col justify-between h-full'>
+                    <div className='text-white xl:p-6 flex flex-col sm:justify-between h-full'>
 
-                        <div className='h-full w-full'>
+                        <div className='h-full w-full hidden md:block'>
                            {/* Spacer Box */}
                         </div>
                         
                         <div className='h-full w-full flex flex-col justify-end'>
-                            <p className='text-lg font-semibold mb-2'>Forecaddie Golf App on IOS and Android</p>
-                            <h2 className='text-5xl font-family-proxima-nova-extra-condensed font-extrabold mb-6 uppercase text-nowrap'>Swing With Confidence</h2>
+                            <p className='text-lg font-semibold mb-2 2xs:text-nowrap'>Forecaddie Golf App on IOS and Android</p>
+                            <h2 className='text-4xl 3xl:text-5xl font-family-proxima-nova-extra-condensed font-extrabold mb-8 uppercase 2xs:text-nowrap'>Swing With Confidence</h2>
                             <div>
                                 <Link href='/' className='bg-white hover:bg-gray-200 transition-all duration-100 ease-in-out-quad text-black px-8 py-2 rounded-full font-semibold cursor-pointer'>
                                     Learn More
@@ -88,14 +88,15 @@ export default function HeroCards() {
                     </div>
 
                     {/* Image */}
-                    <Image
-                        src="/webm/forecaddi.webp"
-                        alt="Forecaddie"
-                        height={506}
-                        width={380}
-                        className="h-auto w-[550px]"
-                    />
-                    
+                    <div className='flex justify-center sm:justify-normal sm:block pt-18 sm:pt-0'>
+                        <Image
+                            src="/webm/forecaddi.webp"
+                            alt="Forecaddie"
+                            height={506}
+                            width={380}
+                            className="h-auto w-[300px] 2xl:w-[550px] 5xl:w-[650px] hidden 2xs:block sm:hidden 2xl:block"
+                        />
+                    </div>
                 </div>
 
             </div>
