d1cba9e6629f32edc16552a6b77bd7e28a064592
SolCaruso <caruso@ocelot.ltd>
2025-07-19 02:50:11 -0400

    fixes ts error. params object is now correctly typed as a promise

--- src/app/shop/[id]/page.tsx
+++ src/app/shop/[id]/page.tsx
@@ -1,11 +1,12 @@
 import ShopItem from "@/components/pages/shop/ShopItem";
 
 interface ShopItemPageProps {
-  params: {
+  params: Promise<{
     id: string;
-  };
+  }>;
 }
 
-export default function ShopItemPage({ params }: ShopItemPageProps) {
-  return <ShopItem productId={parseInt(params.id)} />;
+export default async function ShopItemPage({ params }: ShopItemPageProps) {
+  const { id } = await params;
+  return <ShopItem productId={parseInt(id)} />;
 } 
