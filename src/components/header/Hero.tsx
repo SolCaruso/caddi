47ab5d97536c9bbeb74429d4372e13bd14584b93
SolCaruso <caruso@ocelot.ltd>
2025-07-25 19:53:04 -0400

    adds fetch priority for hero

--- src/components/header/Hero.tsx
+++ src/components/header/Hero.tsx
@@ -17,6 +17,7 @@ export default function Hero() {
                 draggable={false}
                 fill
                 priority
+                fetchPriority="high"
             />
             <div className="absolute inset-0 bg-gradient-to-r from-black/70 to-transparent "></div>
             <div className="absolute inset-0 flex items-center">
